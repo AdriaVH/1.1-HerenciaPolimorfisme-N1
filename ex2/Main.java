cb00f6eeddd3423147231107b8e7abda7363ea19
Adrià <166712306+AdriaVH@users.noreply.github.com>
2025-02-19 10:16:51 +0100

    Create Main.java ex2

---
+++ ex2/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //Ex2
+        Car.model = "Cupra";
+        System.out.println(Car.brand+" "+Car.model);
+        Car myCar = new Car("Leon");
+        System.out.println(myCar.brand+" "+myCar.model);
+
+        System.out.println(myCar.brand+" "+Car.model);
+        System.out.println("myCar Horse power is: "+myCar.horsePower);
+
+        myCar.model = "Ibiza";
+        System.out.println(myCar.brand+" "+myCar.model);
+
+        Car.slowDown();
+        myCar.speedUp();
+    }
+}
